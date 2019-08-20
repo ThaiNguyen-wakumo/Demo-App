50f52e68ed23b913aee172cf2af48654c1d078fe
ThaiNguyen-wakumo <51896221+ThaiNguyen-wakumo@users.noreply.github.com>
2019-08-20 17:44:37 +0700

    Revert "Login and Signup Tab"

--- appDemo/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import { AppRegistry } from "react-native";
-import DatabaseComponent from "./components/DatabaseComponent";
-import { name as appName } from "./app.json";
-
-AppRegistry.registerComponent("appDemo", () => DatabaseComponent);
